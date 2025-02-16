fab71ad3ff3b93598a94e5703a0452beeee37138
umutoniwasejo <umutoniwasejoyeuse2@gmail.com>
2025-02-16 11:04:34 -0800

    Create App.jsx

---
+++ alx-react-app/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Welcome to ALX React App 🚀</h1>
+      <p>This is a simple React app set up with Vite.</p>
+    </div>
+  );
+};
+
+export default App;
